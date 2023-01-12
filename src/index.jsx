dd8c49c59b13239548ce95147af67f5e0048057e
kumarsatyam444 <ndakumarsatyam@gmail.com>
2023-01-12 11:01:31 +0530

    initialized new project and install dev deps

--- src/index.jsx
+++ src/index.jsx
@@ -5,12 +5,16 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+
+//const root = ReactDOM.createRoot(document.getElementById('root'));
+//root.render(
+  //<React.StrictMode>
+    //<App />
+  //</React.StrictMode>
+//);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
@@ -20,4 +24,4 @@ serviceWorkerRegistration.unregister();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
