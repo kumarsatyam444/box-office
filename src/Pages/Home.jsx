9e7bd4ee351b882e7aae63e0deaf1922b85561c9
kumarsatyam444 <ndakumarsatyam@gmail.com>
2023-01-15 20:55:04 +0530

    created preview cards for api data

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,8 @@
 import React, {useState} from 'react';
+import ActorGrid from '../components/actor/ActorGrid';
 import MainPageLayout from '../components/MainPageLayout';
 import { apiGet } from '../misc/config';
+import ShowGrid from '../components/show/ShowGrid';
 
 
 const Home = () => {
@@ -35,11 +37,7 @@ const Home = () => {
       return<div>No results</div>;
     }
     if (results && results.length > 0){
-      return results[0].show ? results.map(item => (
-        <div key={item.show.id}>{item.show.name}</div>
-      )) : results.map(item => (
-        <div key={item.person.id}>{item.person.name}</div>
-      ));
+      return results[0].show ? <ShowGrid /> : <ActorGrid />;
     }
     return null;
   };
