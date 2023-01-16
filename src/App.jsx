5e2a124002d380144177801c307fde7a47d400c7
kumarsatyam444 <ndakumarsatyam@gmail.com>
2023-01-16 21:19:28 +0530

    added loading and error messages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './Pages/Home';
+import Show from './Pages/Show';
 import Starred from './Pages/Starred';
 
 function App() {
@@ -11,8 +12,12 @@ function App() {
         <Home /> 
       </Route>
 
-      <Route exact path="/starred">
+      <Route exact path="/Starred">
         <Starred />
+      </Route>
+      <Route exact path="/show/:id">
+        <Show />
+
       </Route>
 
       <Route><div>NOT FOUND</div></Route>
