1485b94a3180ef4e51f3bdeb2620a6ec42691132
kumarsatyam444 <ndakumarsatyam@gmail.com>
2023-01-21 22:30:20 +0530

    deployed the app

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
@@ -8,7 +8,7 @@ import App from './App';
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
-root.render(<BrowserRouter><App /></BrowserRouter>);
+root.render(<HashRouter><App /></HashRouter>);
 
 
 
