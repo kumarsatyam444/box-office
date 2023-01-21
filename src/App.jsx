f73ce1e54f7816dfb3c285aff43cdaedfe9b5ea2
kumarsatyam444 <ndakumarsatyam@gmail.com>
2023-01-21 21:24:21 +0530

    defined theme for components

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,41 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Home from './Pages/Home';
-import Show from './Pages/Show';
+import { ThemeProvider } from 'styled-components';
+
+import Home from './Pages/Home'
 import Starred from './Pages/Starred';
+import Show from './Pages/Show';
+
+const theme = {
+  mainColors: {
+    blue: '#2400ff',
+    gray: '#c6c6c6',
+    dark: '#353535',
+  },
+};
 
 function App() {
   return (
+    <ThemeProvider theme={theme}>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
 
-    <Switch>
-      <Route exact path="/">
-        <Home /> 
-      </Route>
+        <Route exact path="/starred">
+          <Starred />
+        </Route>
 
-      <Route exact path="/Starred">
-        <Starred />
-      </Route>
-      <Route exact path="/show/:id">
-        <Show />
+        <Route exact path="/show/:id">
+          <Show />
+        </Route>
 
-      </Route>
-
-      <Route><div>NOT FOUND</div></Route>
-    </Switch>
+        <Route>
+          <div>Not found</div>
+        </Route>
+      </Switch>
+    </ThemeProvider>
   );
 }
 
-export default App
+export default App;
